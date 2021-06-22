280168929e76deb162697cd2acb0b85d6949791a
mugri-ndeh <mugrindeh@gmail.com>
2021-06-22 20:16:11 +0100

    learned sets

--- guessing_game.js
+++ guessing_game.js
@@ -1,9 +1,10 @@
 //guessing game
+export function guess(num){
 guess = parseInt(Math.random()*10);
     
 console.log(guess);
 
-var num = window.prompt("Enter a number to guess");
+num = window.prompt("Enter a number to guess");
 
 if(num==guess){
 alert("Correct Guess")
@@ -11,4 +12,5 @@ alert("Correct Guess")
 }
 else{
     alert("Wrong guess try again")
+}
 }
--- script.js
+++ script.js
@@ -1,16 +1,33 @@
-function divide(divisor, divident) {
-    let result = divisor/divident;
-    return result;
+//ES6JS
+
+let add = (num1, num2) => num1+num2;
+
+let fact = (num)=>{
+    if(num===0||num===1){
+        return 1;
+    }
+    else return num*fact(num-1);
 }
 
-let num1 = 0;
-let num2 = 5;
+// js template literals
 
-answer = divide(num2,num1);
+var name = "chrome";
+console.log(`Hello ${name}`);
 
-if(answer!=Infinity){
-    console.log("Correct division");
+var array = ["I", "am", "groot", "yep"];
+
+var array_1 = ["Hello",...array];
+
+console.log(...array_1);
+
+function hello(...ay){
+    console.log(...ay);
 }
-else{
-    throw new Error("Invalid operation on didvion. Divisor cannot be zero")
-}
+
+hello(...array, ...array_1);
+
+let map = new Map();
+
+map.set('map', {"name":"Mugri", "school":"Fet"});
+
+console.log(map.get('map'));
